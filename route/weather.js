c0c44af0fc797e130a07c6b7e460c37f66b9bac8
umerkkr123 <umerkhokhar19@gmail.com>
2025-01-12 17:21:20 +0500

    city fixes

--- route/weather.js
+++ route/weather.js
@@ -1,47 +1,97 @@
-const { formatWeatherResponse } = require('../helper/formatWeather')
+// const { formatWeatherResponse } = require('../helper/formatWeather')
+// require('dotenv').config();
+// const axios = require('axios');
+
+// const weatherRouter = async (req, res) => {
+//     console.log('req.body', req.body)
+//     try {
+//         const intent = (req.body.queryResult?.intent?.displayName !== undefined)
+//             ? req.body.queryResult.intent.displayName
+//             : req.body.intent;
+
+//         const city = (req.body.queryResult?.parameters !== undefined)
+//             ? req.body.queryResult.parameters.geo-city
+//             : req.body.city;
+//         let responseText = '';
+
+//         if (intent === 'GetCurrentWeather') {
+//             const response = await axios.get(`${process.env.OPENWEATHER_BASE_URL}/weather`, {
+//                 params: {
+//                     q: city,
+//                     appid: process.env.OPENWEATHER_API_KEY
+//                 }
+//             });
+//             responseText = formatWeatherResponse(response.data);
+//         }
+//         else if (intent === 'GetWeatherForecast') {
+//             const response = await axios.get(`${process.env.OPENWEATHER_BASE_URL}/forecast`, {
+//                 params: {
+//                     q: city,
+//                     appid: process.env.OPENWEATHER_API_KEY
+//                 }
+//             });
+//             responseText = formatWeatherResponse(response.data, true);
+//         }
+
+//         res.send({
+//             fulfillmentText: responseText
+//         });
+//     } catch (error) {
+//         console.error('Error:', error);
+//         res.send({
+//             fulfillmentText: 'Sorry, I encountered an error while fetching the weather information.'
+//         });
+//     }
+// }
+
+// module.exports = { weatherRouter }
+
+const { formatWeatherResponse } = require('../helper/formatWeather');
 require('dotenv').config();
 const axios = require('axios');
 
 const weatherRouter = async (req, res) => {
-    console.log('req.body', req.body)
-    try {
-        const intent = (req.body.queryResult?.intent?.displayName !== undefined)
-            ? req.body.queryResult.intent.displayName
-            : req.body.intent;
+    console.log('req.body', req.body);
 
-        const city = (req.body.queryResult?.parameters !== undefined)
-            ? req.body.queryResult.parameters.geo-city
-            : req.body.city;
+    try {
+        const intent = req.body.queryResult?.intent?.displayName;
+
+        // Correctly access 'geo-city' parameter
+        const city = req.body.queryResult?.parameters?.['geo-city'];
+
+        if (!city) {
+            throw new Error('City parameter is missing.');
+        }
+
         let responseText = '';
 
         if (intent === 'GetCurrentWeather') {
             const response = await axios.get(`${process.env.OPENWEATHER_BASE_URL}/weather`, {
                 params: {
                     q: city,
-                    appid: process.env.OPENWEATHER_API_KEY
-                }
+                    appid: process.env.OPENWEATHER_API_KEY,
+                },
             });
             responseText = formatWeatherResponse(response.data);
-        }
-        else if (intent === 'GetWeatherForecast') {
+        } else if (intent === 'GetWeatherForecast') {
             const response = await axios.get(`${process.env.OPENWEATHER_BASE_URL}/forecast`, {
                 params: {
                     q: city,
-                    appid: process.env.OPENWEATHER_API_KEY
-                }
+                    appid: process.env.OPENWEATHER_API_KEY,
+                },
             });
             responseText = formatWeatherResponse(response.data, true);
         }
 
-        res.send({
-            fulfillmentText: responseText
+        res.status(200).send({
+            fulfillmentText: responseText,
         });
     } catch (error) {
-        console.error('Error:', error);
-        res.send({
-            fulfillmentText: 'Sorry, I encountered an error while fetching the weather information.'
+        console.error('Error:', error.message);
+        res.status(200).send({
+            fulfillmentText: 'Sorry, I encountered an error while fetching the weather information.',
         });
     }
-}
+};
 
-module.exports = { weatherRouter }
+module.exports = { weatherRouter };
